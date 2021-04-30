0f8178bf7214acd7178306fc8702df678c156f3a
YoBiya <nanashigure@extra.ocn.ne.jp>
2021-04-30 19:35:09 +0900

    空白用のウィジェットを追加

---
+++ shortbread/lib/view/ExpandedSpace.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ExpandedSpace extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Container());
+  }
+}
