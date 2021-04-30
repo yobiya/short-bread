8d3f7a951e7678c20cf933a040c3f0890de7dedd
YoBiya <nanashigure@extra.ocn.ne.jp>
2021-05-01 02:00:10 +0900

    ボックスの説明文表示を追加

--- shortbread/lib/view/SBBoxView.dart
+++ shortbread/lib/view/SBBoxView.dart
@@ -4,16 +4,28 @@ class SBBoxView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "Box",
-            style: Theme.of(context).textTheme.subtitle1,
+      child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Box",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
           ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Description",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
