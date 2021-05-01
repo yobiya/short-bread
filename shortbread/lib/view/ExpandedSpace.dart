074191eee0e75a33fe4ad15fa652a3c43a9ae2b5
YoBiya <nanashigure@extra.ocn.ne.jp>
2021-05-01 15:53:16 +0900

    不要になったコードを削除

--- shortbread/lib/view/ExpandedSpace.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ExpandedSpace extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(child: Container());
-  }
-}
