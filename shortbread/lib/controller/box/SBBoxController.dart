6359780fb0c543263fd26c773b50feef027c9eff
YoBiya <nanashigure@extra.ocn.ne.jp>
2021-05-02 17:49:11 +0900

    ボックスの編集ダイアログが表示できるように修正

--- shortbread/lib/controller/box/SBBoxController.dart
+++ shortbread/lib/controller/box/SBBoxController.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shortbread/model/box/SBBoxModel.dart';
-import 'package:shortbread/view-data/box/SBBoxViewData.dart';
 import 'package:shortbread/view/box/SBBoxListView.dart';
 
 class SBBoxControllerView extends StatefulWidget {
@@ -14,9 +13,6 @@ class SBBoxControllerView extends StatefulWidget {
 class SBBoxController extends State<SBBoxControllerView> {
   SBBoxModel _boxModel = SBBoxModel();
   int selectedId;
-  void Function(int) onChangedSelectedBox = (id) {};
-  void Function(WidgetBuilder) showDialog = (builder) {};
-  void Function() closeDialog = () {};
 
   SBBoxController() {
     var viewData = _boxModel.getViewData();
@@ -34,61 +30,67 @@ class SBBoxController extends State<SBBoxControllerView> {
   }
 
   void _selectBox(int id) {
-    selectedId = id;
-
-    onChangedSelectedBox(id);
-  }
-
-  void _showEditDialog(int id) {
-    this.showDialog((context) {
-      var viewData = _boxModel.getViewData();
-      var data = viewData.singleWhere((data) => data.id == id);
-
-      return AlertDialog(
-        title: Text('Edit box'),
-        content: Container(
-          width: 400,
-          height: 420,
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Title',
-                ),
-                minLines: 1,
-                controller: TextEditingController(text: data.title),
-              ),
-              Container(height: 20),
-              Scrollbar(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Description',
-                  ),
-                  controller: TextEditingController(text: data.description),
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 12,
-                ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: closeDialog,
-            child: Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: _closeDialogAndSave,
-            child: Text('OK'),
-          ),
-        ],
-      );
+    setState(() {
+      selectedId = id;
     });
   }
 
+  void _showEditDialog(int id) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          var viewData = _boxModel.getViewData();
+          var data = viewData.singleWhere((data) => data.id == id);
+
+          return AlertDialog(
+            title: Text('Edit box'),
+            content: Container(
+              width: 400,
+              height: 420,
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Title',
+                    ),
+                    minLines: 1,
+                    controller: TextEditingController(text: data.title),
+                  ),
+                  Container(height: 20),
+                  Scrollbar(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Description',
+                      ),
+                      controller: TextEditingController(text: data.description),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: _closeDialog,
+                child: Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: _closeDialogAndSave,
+                child: Text('OK'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void _closeDialog() {
+    Navigator.of(context).pop(context);
+  }
+
   void _closeDialogAndSave() {
-    closeDialog();
+    _closeDialog();
   }
 }
